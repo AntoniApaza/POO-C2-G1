af40a603f46a542d89cc5a93f1e6fdb6cfce7b66
AntoniApaza <antoniapaza64@gmail.com>
2025-08-25 10:18:57 -0500

    Add files via upload

---
+++ Aplication.java
@@ -0,0 +1,8 @@
+package pe.edu.upeu.asistncia;
+
+public class Aplication {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        AsistnciaApplication.main(args);
+    }
+}
